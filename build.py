99f394e16d70a50eab744ecd5eec7f4aa5a751a2
KodeWorker <fxp61005@gmail.com>
2019-07-31 19:03:16 +0800

    20190731 update

---
+++ build.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+
+import os
+import shutil
+
+if __name__ == '__main__':
+    # 清空前次編譯
+    if os.path.exists('main.spec'):
+        os.remove('main.spec')
+    if os.path.exists('build'):
+        shutil.rmtree('build')
+    if os.path.exists('dist'):
+        shutil.rmtree('dist')
+    # 編譯程式
+    os.system('pyinstaller main.py')
+    os.rename(os.path.join(os.path.dirname(__file__), 'dist', 'main', 'PyQt5', 'Qt', 'plugins', 'platforms'),
+              os.path.join(os.path.dirname(__file__), 'dist', 'main', 'platforms'),)
+    # 執行測試
+    os.system('{}'.format(os.path.abspath('dist/main/main.exe')))
---
+++ readme.md
@@ -0,0 +1,10 @@
+# 3D Model Analysis v 0.0.1
+
+## Project Goal
+
+## Dev Log
+- 2019/07/31 project initiation
+
+## Reference
+
+## Acknowledgement
