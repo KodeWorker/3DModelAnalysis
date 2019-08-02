ee170858b46559c100853f80633270170eafa612
KodeWorker <fxp61005@gmail.com>
2019-08-02 18:12:11 +0800

    20190802 update

--- test_occ4.py
+++ test_occ4.py
@@ -18,13 +18,12 @@ import ifcopenshell.geom
 from OCC.TopoDS import topods
 #from OCC.BRepTools import BRepTools_WireExplorer
 
-#import matplotlib.pyplot as plt
 
 from core_topology_traverse import Topo
 from core_geometry_bounding_box import get_boundingbox
 
 # RGBA colors for the visualisation of elements
-RED, WHITE = (1.0, 0, 0, 1.0), (1.0, 1.0, 1.0, 1.0)
+RED, WHITE = (1.0, 0.0, 0.0, 1.0), (1.0, 1.0, 1.0, 1.0)
 
 # Specify to return pythonOCC shapes from ifcopenshell.geom.create_shape()
 settings = ifcopenshell.geom.settings()
@@ -45,10 +44,6 @@ xmin, ymin, zmin, xmax, ymax, zmax, x_range, y_range, z_range = get_boundingbox(
 
 #section_height = zmin+1e-3
 section_height = zmax-1e-3
-
-#plt.figure()
-#plt.xlim((xmin, xmax))
-#plt.ylim((ymin, ymax))
 
 # A horizontal plane is created from which a face is constructed to intersect with 
 # the building. The face is transparently displayed along with the building.    
@@ -73,31 +68,8 @@ while exp.More():
         section = OCC.BRepAlgoAPI.BRepAlgoAPI_Section(section_face, face).Shape()
         section_edges = list(Topo(section).edges())
         
-        if len(section_edges) > 0:
-            # Open Cascade has a function to turn loose unconnected edges into a list of 
-            # connected wires. This function takes handles (pointers) to Open Cascade's native 
-            # sequence type. Hence, two sequences and handles, one for the input, one for the 
-            # output, are created. 
-            edges = OCC.TopTools.TopTools_HSequenceOfShape()
-            edges_handle = OCC.TopTools.Handle_TopTools_HSequenceOfShape(edges)
-            
-            wires = OCC.TopTools.TopTools_HSequenceOfShape()
-            wires_handle = OCC.TopTools.Handle_TopTools_HSequenceOfShape(wires)
-            
-            # The edges are copied to the sequence
-            for edge in section_edges: edges.Append(edge)
-                        
-            # A wire is formed by connecting the edges
-            OCC.ShapeAnalysis.ShapeAnalysis_FreeBounds.ConnectEdgesToWires(edges_handle, 1e-5, True, wires_handle)
-            wires = wires_handle.GetObject()
-            
-            for i in range(wires.Length()):
-                wire_shape = wires.Value(i+1)
-                wire = topods.Wire(wire_shape)
-                face = OCC.BRepBuilderAPI.BRepBuilderAPI_MakeFace(wire).Face()                
-                # The wires and the faces are displayed
-                ifcopenshell.geom.utils.display_shape(wire, clr=RED)
-                face_display = ifcopenshell.geom.utils.display_shape(face, clr=RED)
-                ifcopenshell.geom.utils.set_shape_transparency(face_display, 0.5)
-                    
+        for edge in section_edges: 
+            ifcopenshell.geom.utils.display_shape(edge, clr=RED)
+
+                
     exp.Next()
---
+++ test_occ_get_curve_params.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+# z-direction cross section
+
+import os
+import warnings
+
+import OCC.gp
+import OCC.BRepAlgoAPI
+import OCC.ShapeAnalysis
+from OCC.STEPControl import STEPControl_Reader
+from OCC.TopAbs import TopAbs_FACE
+from OCC.TopExp import TopExp_Explorer
+from OCC.BRep import BRep_Tool
+from OCC.Geom2dAdaptor import Geom2dAdaptor_Curve
+from OCC.GeomAbs import (GeomAbs_Line, GeomAbs_Circle, GeomAbs_Ellipse, 
+                         GeomAbs_Hyperbola, GeomAbs_Parabola, 
+                         GeomAbs_BezierCurve, GeomAbs_BSplineCurve,
+                         GeomAbs_OtherCurve)
+from OCC.GCPnts import GCPnts_AbscissaPoint_Length
+
+from core_topology_traverse import Topo
+from core_geometry_bounding_box import get_boundingbox
+
+import ifcopenshell
+import ifcopenshell.geom
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+from math import atan2, pi
+
+def angle360(vector1, vector2=(1, 0)):
+#    dot = vector1[0]*vector2[0] + vector1[1]*vector2[1]      # dot product
+#    det = vector1[0]*vector2[1] - vector1[1]*vector2[0]      # determinant
+#    angle = atan2(det, dot)  # atan2(y, x) or atan2(sin, cos)
+    
+    import numpy as np
+    angle = np.degrees(np.arctan2(vector1[1], vector1[0]))
+    return angle
+
+
+# RGBA colors for the visualisation of elements
+RED, WHITE = (1.0, 0.0, 0.0, 1.0), (1.0, 1.0, 1.0, 1.0)
+
+# Specify to return pythonOCC shapes from ifcopenshell.geom.create_shape()
+settings = ifcopenshell.geom.settings()
+settings.set(settings.USE_PYTHON_OPENCASCADE, True)
+
+# Initialize a graphical display window
+occ_display = ifcopenshell.geom.utils.initialize_display()
+#occ_display.View.SetBackgroundColor(WHITE)
+
+# Read the file and get the shape
+reader = STEPControl_Reader()
+tr = reader.WS().GetObject().TransferReader().GetObject()
+reader.ReadFile(os.path.abspath(os.path.join('.', 'models', 'TPI_PH_CNF95XX.STEP')))
+reader.TransferRoots()
+shape = reader.OneShape()
+
+xmin, ymin, zmin, xmax, ymax, zmax, x_range, y_range, z_range = get_boundingbox(shape)
+
+#section_height = zmin+1e-3
+section_height = zmax-1e-3
+
+plt.figure()
+plt.xlim((xmin, xmax))
+plt.ylim((ymin, ymax))
+
+# A horizontal plane is created from which a face is constructed to intersect with 
+# the building. The face is transparently displayed along with the building.    
+section_plane = OCC.gp.gp_Pln(
+    OCC.gp.gp_Pnt(0, 0, section_height),
+    OCC.gp.gp_Dir(0, 0, 1)
+)
+section_face = OCC.BRepBuilderAPI.BRepBuilderAPI_MakeFace(section_plane, xmin, xmax, ymin, ymax).Face()
+
+n_edges = 0
+
+# Explore the faces of the shape (these are known to be named)
+exp = TopExp_Explorer(shape, TopAbs_FACE)
+while exp.More():
+    s = exp.Current()
+    
+    tp = Topo(s)
+    for face in tp.faces():
+            
+        section = OCC.BRepAlgoAPI.BRepAlgoAPI_Section(section_face, face).Shape()
+        section_edges = list(Topo(section).edges())
+        
+        for edge in section_edges:
+            
+            curve_handle, first, last = BRep_Tool.CurveOnSurface(edge, section_face)
+            handle_adaptor = Geom2dAdaptor_Curve(curve_handle)
+#            length = GCPnts_AbscissaPoint_Length(handle_adaptor)
+            
+            if handle_adaptor.GetType() == GeomAbs_Line:
+                v = list(Topo(edge).vertices())
+                x1, y1 = BRep_Tool.Pnt(v[0]).X(), BRep_Tool.Pnt(v[0]).Y()
+                x2, y2 = BRep_Tool.Pnt(v[-1]).X(), BRep_Tool.Pnt(v[-1]).Y()
+                plt.plot([x1, x2], [y1, y2], color="red")
+                
+            elif handle_adaptor.GetType() == GeomAbs_Circle:
+#                print("CIRCLE")
+                v = list(Topo(edge).vertices())
+                start = (BRep_Tool.Pnt(v[0]).X(), BRep_Tool.Pnt(v[0]).Y())
+                end = (BRep_Tool.Pnt(v[-1]).X(), BRep_Tool.Pnt(v[-1]).Y())
+                
+                circle = handle_adaptor.Circle()
+                center = (circle.Location().X(), circle.Location().Y())
+                radius = circle.Radius()
+                
+                vec_start = (start[0] - center[0], start[1] - center[1])
+                vec_end = (end[0] - center[0], end[1] - center[1])
+                
+                t_start = angle360(vec_start)
+                t_end = angle360(vec_end)
+                
+                t_start, t_end = t_end, t_start
+#                t_start, t_end = min(t_start, t_end), max(t_start, t_end)
+                    
+#                print(t_start, t_end)
+                circle_width, circle_height = 2*radius, 2*radius
+                arc = mpatches.Arc(xy=center, width=circle_width, 
+                                   height=circle_height, angle=0,
+                                   theta1=t_start, theta2=t_end, 
+                                   color="red")
+                plt.gca().add_patch(arc)
+                
+            else:
+                print(handle_adaptor.GetType())
+                warnings.warn("Not recognized curve!")
+            
+            ifcopenshell.geom.utils.display_shape(edge, clr=RED)
+            n_edges += 1
+
+                
+    exp.Next()
